9ead6dd1b5eb02eda7f1ba2af8c80d5f4ebd19b1
Anupam Singh <singh.anupam@in.ibm.com>
2026-02-14 15:17:21 +0530

    remove multiply code

--- src/main/java/com/example/MathService.java
+++ src/main/java/com/example/MathService.java
@@ -8,10 +8,5 @@ public class MathService {
     public int subtract(int a, int b) {
         return a - b;
     }
-    
-    // New feature added by Developer
-    public int multiply(int a, int b) {
-        return a * b;
-    }
 	
 }
