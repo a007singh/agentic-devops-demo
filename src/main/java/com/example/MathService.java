18180e51d27f46a3be226d736891e5aadc0fc3db
a007singh <a007singh@users.noreply.github.com>
2026-02-15 10:10:01 +0530

    Add multiply method to MathService

--- src/main/java/com/example/MathService.java
+++ src/main/java/com/example/MathService.java
@@ -8,6 +8,11 @@ public class MathService {
     public int subtract(int a, int b) {
         return a - b;
     }
+
+	// New feature added by Developer
+    public int multiply(int a, int b) {
+        return a * b;
+    }
     
 	
 }
